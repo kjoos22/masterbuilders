c06e61ca3cc0d707e3321ef06ba8af4bd2598a2c
kjoos22 <kjoos22@gmail.com>
2021-02-27 14:20:00 -0500

    added oauth login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
     def omniauth
+        #provider = auth['provider']
+        #name = auth['info']['name']
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+        end
+    end
 
+    private
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
