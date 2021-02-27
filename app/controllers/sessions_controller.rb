f2988318adf1a7a3fc03b984368649db90bd3b07
kjoos22 <kjoos22@gmail.com>
2021-02-27 15:29:58 -0500

    built out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,17 @@ class SessionsController < ApplicationController
     def omniauth
         #provider = auth['provider']
         #name = auth['info']['name']
-        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+        user = User.find_or_create_by(uid: auth['uid'],
+                provider: auth['provider']) do |u|
             u.username = auth['info']['name']
             u.email = auth['info']['email']
         end
+
+        if user.valid?
+            redirect_to 
+        else
+            flash[:message] = user.errors.full_messages.join("")
+            redirect_to
     end
 
     private
