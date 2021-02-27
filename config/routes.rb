8c3386457ec2d04341784f597aea5496cf76b6a0
kjoos22 <kjoos22@gmail.com>
2021-02-27 16:50:30 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/users/:id/difficult', to: 'users#difficult', as: 'difficult_sets'
   get '/auth/:provider/callback', to: 'sessions#omniauth'
+  
+  resources :users
+  resources :legosets 
+
 end
